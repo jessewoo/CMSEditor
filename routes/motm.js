66eb773ae5c843d12b22c8bc7e5b4934f50c5d7b
Chris Randle <chris.w.randle@gmail.com>
2015-11-13 14:00:55 -0800

    Nothing big.

--- routes/motm.js
+++ routes/motm.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET motm listing. */
 router.get('/', function(req, res, next) {
   var db = req.db;
   var collection = db.get('motm_articles');
