aacc90e99b06e8a09adcbd0d0e9394d278bcaa49
Chris Randle <chris.w.randle@gmail.com>
2015-11-16 08:56:10 -0800

    More minor stuff.

--- routes/motm.js
+++ routes/motm.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET motm listing. */
+//Base for server:3000/motm/
 router.get('/', function(req, res, next) {
   var db = req.db;
   var collection = db.get('motm_articles');
@@ -12,6 +13,16 @@ router.get('/', function(req, res, next) {
     });
   });
   //res.send('respond with a resource');
+});
+
+//Make a new route under server:3000/motm/STUFF
+router.get('/count', function(request,response,next) {
+  var db = request.db;
+  var collection = db.get('motm_articles');
+  collection.find({_momID:o_motmID},{},function(error,cursor){
+    cursor.toArray(callback);
+    //response.render('motm', {"motm_articles" : docs });
+  });
 });
 
 module.exports = router;
