84342f64e450ee245cef5d0dbe8981e6beaee670
Cole Christie <colec.mobile@gmail.com>
2015-12-16 06:30:12 -0800

    image uploads work & have good names

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,32 @@
 var express = require('express');
 var router = express.Router();
 var multer = require('multer');
-var upload = multer({ dest: './image-uploads'}).single('userPhoto');
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './image-uploads')
+    },
+    filename: function (req, file, cb) {
+        console.log("Filename:", file)
+        cb(null, file.originalname)
+    }
+})
+var upload = multer({storage: storage}).single('image-upload');
 
 // Base home page
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CMS Editor' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'CMS Editor'});
 });
 
 // Image upload test code
-router.get('/image', function(req, res, next){
-   res.render('image', { title: 'Image Upload'});
+router.get('/image', function (req, res, next) {
+    res.render('image', {title: 'Image Upload'});
 });
 
 //======================= IMAGE WORK START
 // Create image file
-router.post('/sendImage', function(req, res){
-    upload(req,res,function(err) {
-        if(err) {
+router.post('/sendImage', function (req, res) {
+    upload(req, res, function (err) {
+        if (err) {
             return res.end("Error uploading file.");
         }
         res.end("File is uploaded");
